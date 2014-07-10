215323175dd72b8453db0365f7a584d741ca154d
Joel Jensen <yobert@gmail.com>
2014-07-09 22:41:36 -0600

    Figured out my deadlock problems (error channel was blocking)

--- main.go
+++ main.go
@@ -3,67 +3,19 @@ package main
 import (
 	"fmt"
 	"os"
-	"errors"
 )
 
 func main() {
 	fmt.Println("starting up")
-	queue := make(chan string, 8196)
+	queue := make(chan string)
 
-	_ = watch(queue)
+	watch(queue)
 
-//	for {
-		// every day, do a full re-index
-//	}
-//	fichan := make(chan []os.FileInfo, 4096)
-
-	for _, path := range os.Args[1:] {
-		queue<-path
-	}
-
-
-//		err := walk(path)
-//		if err != nil {
-//			fmt.Println("ERROR: ", err)
-//		}
-//		err := watch_add(path)
-//		if err != nil {
-//			fmt.Println("ERROR: ", err)
-//		}
-//	}
+	go func() {
+		for _, path := range os.Args[1:] {
+			queue <- path
+		}
+	}()
 
 	select {}
 }
-
-func walk(path string) error {
-	pi, err := os.Stat(path)
-	if err != nil {
-		return err
-	}
-
-	if !pi.IsDir() {
-		return errors.New("Not a directory: "+path)
-	}
-
-	f, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-
-	fi, err := f.Readdir(0)
-	if err != nil {
-		return err
-	}
-
-	for _, ffi := range fi {
-		if ffi.IsDir() {
-			err := walk(path + "/" + ffi.Name())
-			if err != nil {
-				return err
-			}
-		}
-	}
-
-	return nil
-}
-
--- watch.go
+++ watch.go
@@ -2,75 +2,89 @@ package main
 
 import (
 	"code.google.com/p/go.exp/fsnotify"
+	"errors"
 	"log"
 	"os"
-	"errors"
+	//	"time"
 )
 
 var watcher *fsnotify.Watcher
 var watched map[string]struct{}
 
-func watch(queue chan string) chan *fsnotify.FileEvent {
+func watch(queue chan string) (chan *fsnotify.FileEvent, error) {
 
 	watched = make(map[string]struct{})
 
 	var err error
 	watcher, err = fsnotify.NewWatcher()
 	if err != nil {
-		log.Fatal(err)
+		return nil, err
 	}
 
 	go func() {
 		for {
 			select {
 			case ev := <-watcher.Event:
-					log.Println("event:", ev)
-					if ev.IsDelete() || ev.IsRename() {
-						_, ok := watched[ev.Name]
-						if ok {
-							delete(watched, ev.Name)
-							log.Println("removing path: "+ev.Name)
-							err := watcher.RemoveWatch(ev.Name)
-							if err != nil {
-								watcher.Error<-err
-								break
-							}
-						}
-					} else if ev.IsCreate() {
-						pi, err := os.Stat(ev.Name)
+				log.Println("event:", ev)
+				if ev.IsDelete() || ev.IsRename() {
+					_, ok := watched[ev.Name]
+					if ok {
+						delete(watched, ev.Name)
+						log.Println("removing path: " + ev.Name)
+						err := watcher.RemoveWatch(ev.Name)
 						if err != nil {
-							watcher.Error<-err
+							go func() {
+								watcher.Error <- err
+							}()
 							break
 						}
-						if pi.IsDir() {
-							queue<-ev.Name
-						}
 					}
+				} else if ev.IsCreate() {
+					pi, err := os.Stat(ev.Name)
+					if err != nil {
+						go func() {
+							watcher.Error <- err
+						}()
+						break
+					}
+					if pi.IsDir() {
+						go func() {
+							queue <- ev.Name
+						}()
+					}
+				}
 			case path := <-queue:
 				_, ok := watched[path]
 				if ok {
-					log.Println("already watching "+path)
+					log.Println("already watching " + path)
 				} else {
-					log.Println("adding path: "+path)
+					log.Println(len(watched), " ", path)
 					watched[path] = struct{}{}
 					we := watcher.Watch(path)
 					if we != nil {
-						watcher.Error<-we
+						go func() {
+							watcher.Error <- we
+						}()
 						break
 					}
-					we = watch_walk(path, queue)
-					if we != nil {
-						watcher.Error<-we
-						break
-					}
+					go func() {
+						//time.Sleep(time.Millisecond * 1000)
+						we = watch_walk(path, queue)
+						if we != nil {
+							go func() {
+								watcher.Error <- we
+							}()
+							return
+						}
+					}()
 				}
 			case err := <-watcher.Error:
-					log.Println("error:", err)
+				log.Println("error:", err)
 			}
 		}
 	}()
 
-	return watcher.Event
+	return watcher.Event, nil
 }
 
 func watch_walk(path string, queue chan string) error {
@@ -80,7 +94,7 @@ func watch_walk(path string, queue chan string) error {
 	}
 
 	if !pi.IsDir() {
-		return errors.New("Not a directory: "+path)
+		return errors.New("Not a directory: " + path)
 	}
 
 	f, err := os.Open(path)
@@ -95,11 +109,9 @@ func watch_walk(path string, queue chan string) error {
 
 	for _, ffi := range fi {
 		if ffi.IsDir() {
-//			err := walk(path + "/" + ffi.Name())
-//			if err != nil {
-//				return err
-//			}
+			//log.Println("queuing", path+"/"+ffi.Name())
 			queue <- path + "/" + ffi.Name()
+			//log.Println("done")
 		}
 	}
 
